3cdcde8ac7474340a77c9d350fd3570d477d58f3
Ahmet <akose2005@gmail.com>
2025-03-23 16:43:21 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,23 @@
+﻿function showSection(sectionId) {
+    var sections = document.getElementsByClassName('section');
+    for(var i = 0; i < sections.length; i++) {
+        sections[i].classList.remove('active');
+    }
+    document.getElementById(sectionId).classList.add('active');
+}
+
+function toggleSidebar() {
+    var sidebar = document.getElementById("sidebar");
+    var openbtn = document.getElementById("openbtn");
+    var content = document.getElementById("content");
+    
+    if (sidebar.style.width === "0px" || sidebar.style.width === "0") {
+        sidebar.style.width = "250px";
+        content.style.marginLeft = "250px";
+        openbtn.style.display = "none";
+    } else {
+        sidebar.style.width = "0";
+        content.style.marginLeft = "0";
+        openbtn.style.display = "block";
+    }
+}
